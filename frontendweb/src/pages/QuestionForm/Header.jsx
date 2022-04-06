71be6999918f0e9afe424a78e2361b7d95760d64
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-04-06 20:43:16 +0530

    doc header setup

--- frontendweb/src/pages/QuestionForm/Header.jsx
+++ frontendweb/src/pages/QuestionForm/Header.jsx
@@ -4,18 +4,23 @@ function Header() {
   return (
     <div className="header">
       <div className="header_info">
-      <button className="rounded-full">Save Changes</button>
-      <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+        <button>nav-icons</button>
+        <button>doc-icons</button>
       </div>
       <div className="header_search">
-      <button className="rounded-full">Save Changes</button>
-        <input type="text" name="search" />
+        <button className="">search</button>
+        
+        <label class="relative block">
+  <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+    <svg class="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"/>
+  </span>
+  <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search for anything..." type="text" name="search"/>
+</label>
+        
       </div>
       <div className="header_right">
-      <button className="rounded-full">Save Changes</button>
-
+        <button className="">AppIcon</button>
+        <button className="">Avatar</button>
       </div>
     </div>
   );
